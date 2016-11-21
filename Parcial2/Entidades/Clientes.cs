9590149cfa4e37a4d0ab593d468458a81c57cc96
Roberti Yavier Amarante Garcia <yavier.ag@gmail.com>
2016-11-21 17:58:46 -0400

    Comenzar

---
+++ Parcial2/Entidades/Clientes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entidades
+{
+    public class Clientes
+    {
+        public int ClienteId { get; set; }
+
+        public string Nombres { get; set; }
+
+        public DateTime FechaNacimiento { get; set; }
+
+        public Double LimiteCredito { get; set; }
+    }
+}
